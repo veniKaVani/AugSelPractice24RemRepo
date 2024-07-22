f6aab1da825bd50cd052301913f3f795a98a2a11
veniKaVani <veniKa2024@gmail.com>
2024-07-22 16:01:01 -0400

    first commit

--- src/main/java/navnewseriessel/FindingAllSocialLinks18b.java
+++ src/main/java/navnewseriessel/FindingAllSocialLinks18b.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//HW Q: How do you each social link one at a time??
+//HW Q: How do you handle each social link one at a time??
 public class FindingAllSocialLinks18b {
         static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException {
